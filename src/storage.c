de387d439f75c9b7f6e1eebe1bf136a3a3077bf3
HyeYeonLee0112 <aa99509188@gmail.com>
2026-04-15 11:32:37 +0900

    refactor: implement B+ Tree index structure for improved ID management and retrieval

--- src/storage.c
+++ src/storage.c
@@ -5,84 +5,259 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define INDEX_INITIAL_CAPACITY 1024
-#define INDEX_LOAD_NUM 7
-#define INDEX_LOAD_DEN 10
+#define BPTREE_MAX_KEYS 31
 #define MAX_BINARY_FIELDS 4096
 #define MAX_BINARY_FIELD_BYTES (16U * 1024U * 1024U)
 
-typedef struct {
-    uint64_t id;
-    RowRef ref;
-    bool used;
-} IndexBucket;
+typedef struct BptNode BptNode;
 
-typedef struct {
-    IndexBucket *buckets;
-    size_t bucket_count;
+struct BptNode {
+    bool is_leaf;
     size_t size;
-} IdIndex;
+    uint64_t keys[BPTREE_MAX_KEYS];
+    union {
+        struct {
+            RowRef refs[BPTREE_MAX_KEYS];
+            BptNode *next;
+        } leaf;
+        struct {
+            BptNode *children[BPTREE_MAX_KEYS + 1];
+        } internal;
+    } as;
+};
 
-static IdIndex g_index;
+typedef struct {
+    bool split;
+    uint64_t promoted_key;
+    BptNode *right;
+} BptSplitResult;
+
+static BptNode *g_index_root = NULL;
 static bool g_index_ready = false;
 static uint64_t g_next_id_counter = 0;
 static char *g_active_data_path = NULL;
 static const TableDefinition *g_active_table = NULL;
 
-static uint64_t hash_u64(uint64_t value) {
-    value ^= value >> 30;
-    value *= 0xbf58476d1ce4e5b9ULL;
-    value ^= value >> 27;
-    value *= 0x94d049bb133111ebULL;
-    value ^= value >> 31;
-    return value;
-}
-
-static void index_free(IdIndex *index) {
-    free(index->buckets);
-    index->buckets = NULL;
-    index->bucket_count = 0;
-    index->size = 0;
-}
-
-static bool index_allocate(IdIndex *index, size_t bucket_count) {
-    index->buckets = (IndexBucket *) calloc(bucket_count, sizeof(IndexBucket));
-    if (index->buckets == NULL) {
-        return false;
+static BptNode *bpt_create_node(bool is_leaf) {
+    BptNode *node = (BptNode *) calloc(1, sizeof(BptNode));
+    if (node == NULL) {
+        return NULL;
     }
-    index->bucket_count = bucket_count;
-    index->size = 0;
-    return true;
+    node->is_leaf = is_leaf;
+    node->size = 0;
+    node->as.leaf.next = NULL;
+    return node;
 }
 
-static bool index_rehash(IdIndex *index, size_t new_bucket_count) {
+static void bpt_free_node(BptNode *node) {
     size_t i;
-    IdIndex new_index;
 
-    memset(&new_index, 0, sizeof(new_index));
-    if (!index_allocate(&new_index, new_bucket_count)) {
-        return false;
+    if (node == NULL) {
+        return;
     }
 
-    for (i = 0; i < index->bucket_count; ++i) {
-        if (index->buckets[i].used) {
-            size_t slot = (size_t) (hash_u64(index->buckets[i].id) % new_index.bucket_count);
-            while (new_index.buckets[slot].used) {
-                slot = (slot + 1) % new_index.bucket_count;
-            }
-            new_index.buckets[slot] = index->buckets[i];
-            new_index.size++;
+    if (!node->is_leaf) {
+        for (i = 0; i <= node->size; ++i) {
+            bpt_free_node(node->as.internal.children[i]);
         }
     }
 
-    index_free(index);
-    *index = new_index;
-    return true;
+    free(node);
+}
+
+static int bpt_find(BptNode *root, uint64_t id, RowRef *out_ref) {
+    BptNode *node = root;
+    size_t i;
+
+    while (!node->is_leaf) {
+        i = 0;
+        while (i < node->size && id >= node->keys[i]) {
+            i++;
+        }
+        node = node->as.internal.children[i];
+        if (node == NULL) {
+            return -1;
+        }
+    }
+
+    for (i = 0; i < node->size; ++i) {
+        if (node->keys[i] == id) {
+            *out_ref = node->as.leaf.refs[i];
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+static int bpt_insert_recursive(BptNode *node, uint64_t id, RowRef ref, BptSplitResult *out_split) {
+    size_t i;
+
+    memset(out_split, 0, sizeof(*out_split));
+
+    if (node->is_leaf) {
+        uint64_t merged_keys[BPTREE_MAX_KEYS + 1];
+        RowRef merged_refs[BPTREE_MAX_KEYS + 1];
+        size_t insert_at = 0;
+        BptNode *right;
+        size_t total;
+        size_t left_count;
+        size_t right_count;
+
+        while (insert_at < node->size && node->keys[insert_at] < id) {
+            insert_at++;
+        }
+
+        if (insert_at < node->size && node->keys[insert_at] == id) {
+            return 1;
+        }
+
+        if (node->size < BPTREE_MAX_KEYS) {
+            for (i = node->size; i > insert_at; --i) {
+                node->keys[i] = node->keys[i - 1];
+                node->as.leaf.refs[i] = node->as.leaf.refs[i - 1];
+            }
+            node->keys[insert_at] = id;
+            node->as.leaf.refs[insert_at] = ref;
+            node->size++;
+            return 0;
+        }
+
+        total = node->size + 1;
+        for (i = 0; i < insert_at; ++i) {
+            merged_keys[i] = node->keys[i];
+            merged_refs[i] = node->as.leaf.refs[i];
+        }
+        merged_keys[insert_at] = id;
+        merged_refs[insert_at] = ref;
+        for (i = insert_at; i < node->size; ++i) {
+            merged_keys[i + 1] = node->keys[i];
+            merged_refs[i + 1] = node->as.leaf.refs[i];
+        }
+
+        left_count = total / 2;
+        right_count = total - left_count;
+        right = bpt_create_node(true);
+        if (right == NULL) {
+            return -1;
+        }
+
+        for (i = 0; i < left_count; ++i) {
+            node->keys[i] = merged_keys[i];
+            node->as.leaf.refs[i] = merged_refs[i];
+        }
+        node->size = left_count;
+
+        for (i = 0; i < right_count; ++i) {
+            right->keys[i] = merged_keys[left_count + i];
+            right->as.leaf.refs[i] = merged_refs[left_count + i];
+        }
+        right->size = right_count;
+
+        right->as.leaf.next = node->as.leaf.next;
+        node->as.leaf.next = right;
+
+        out_split->split = true;
+        out_split->promoted_key = right->keys[0];
+        out_split->right = right;
+        return 0;
+    }
+
+    i = 0;
+    while (i < node->size && id >= node->keys[i]) {
+        i++;
+    }
+
+    {
+        BptSplitResult child_split;
+        int rc = bpt_insert_recursive(node->as.internal.children[i], id, ref, &child_split);
+        if (rc != 0) {
+            return rc;
+        }
+
+        if (!child_split.split) {
+            return 0;
+        }
+
+        if (node->size < BPTREE_MAX_KEYS) {
+            size_t j;
+
+            for (j = node->size; j > i; --j) {
+                node->keys[j] = node->keys[j - 1];
+            }
+            for (j = node->size + 1; j > i + 1; --j) {
+                node->as.internal.children[j] = node->as.internal.children[j - 1];
+            }
+
+            node->keys[i] = child_split.promoted_key;
+            node->as.internal.children[i + 1] = child_split.right;
+            node->size++;
+            return 0;
+        }
+
+        {
+            uint64_t merged_keys[BPTREE_MAX_KEYS + 1];
+            BptNode *merged_children[BPTREE_MAX_KEYS + 2];
+            BptNode *right;
+            size_t total_keys;
+            size_t mid;
+            size_t left_keys;
+            size_t right_keys;
+            size_t j;
+
+            for (j = 0; j < i; ++j) {
+                merged_keys[j] = node->keys[j];
+            }
+            merged_keys[i] = child_split.promoted_key;
+            for (j = i; j < node->size; ++j) {
+                merged_keys[j + 1] = node->keys[j];
+            }
+
+            for (j = 0; j <= i; ++j) {
+                merged_children[j] = node->as.internal.children[j];
+            }
+            merged_children[i + 1] = child_split.right;
+            for (j = i + 1; j <= node->size; ++j) {
+                merged_children[j + 1] = node->as.internal.children[j];
+            }
+
+            total_keys = node->size + 1;
+            mid = total_keys / 2;
+            left_keys = mid;
+            right_keys = total_keys - mid - 1;
+
+            right = bpt_create_node(false);
+            if (right == NULL) {
+                return -1;
+            }
+
+            for (j = 0; j < left_keys; ++j) {
+                node->keys[j] = merged_keys[j];
+                node->as.internal.children[j] = merged_children[j];
+            }
+            node->as.internal.children[left_keys] = merged_children[left_keys];
+            node->size = left_keys;
+
+            for (j = 0; j < right_keys; ++j) {
+                right->keys[j] = merged_keys[mid + 1 + j];
+                right->as.internal.children[j] = merged_children[mid + 1 + j];
+            }
+            right->as.internal.children[right_keys] = merged_children[total_keys];
+            right->size = right_keys;
+
+            out_split->split = true;
+            out_split->promoted_key = merged_keys[mid];
+            out_split->right = right;
+            return 0;
+        }
+    }
 }
 
 int index_init(void) {
-    index_free(&g_index);
-    if (!index_allocate(&g_index, INDEX_INITIAL_CAPACITY)) {
+    bpt_free_node(g_index_root);
+    g_index_root = bpt_create_node(true);
+    if (g_index_root == NULL) {
+        g_index_ready = false;
         return -1;
     }
     g_index_ready = true;
@@ -90,55 +265,39 @@ int index_init(void) {
 }
 
 int index_insert(uint64_t id, RowRef ref) {
-    size_t slot;
+    BptSplitResult split;
+    int rc;
 
-    if (!g_index_ready) {
+    if (!g_index_ready || g_index_root == NULL) {
         return -1;
     }
 
-    if ((g_index.size + 1) * INDEX_LOAD_DEN > g_index.bucket_count * INDEX_LOAD_NUM) {
-        if (!index_rehash(&g_index, g_index.bucket_count * 2)) {
+    rc = bpt_insert_recursive(g_index_root, id, ref, &split);
+    if (rc != 0) {
+        return rc;
+    }
+
+    if (split.split) {
+        BptNode *new_root = bpt_create_node(false);
+        if (new_root == NULL) {
             return -1;
         }
+        new_root->keys[0] = split.promoted_key;
+        new_root->size = 1;
+        new_root->as.internal.children[0] = g_index_root;
+        new_root->as.internal.children[1] = split.right;
+        g_index_root = new_root;
     }
 
-    slot = (size_t) (hash_u64(id) % g_index.bucket_count);
-    while (g_index.buckets[slot].used) {
-        if (g_index.buckets[slot].id == id) {
-            return 1;
-        }
-        slot = (slot + 1) % g_index.bucket_count;
-    }
-
-    g_index.buckets[slot].id = id;
-    g_index.buckets[slot].ref = ref;
-    g_index.buckets[slot].used = true;
-    g_index.size++;
     return 0;
 }
 
 int index_find(uint64_t id, RowRef *out_ref) {
-    size_t slot;
-    size_t scanned = 0;
-
-    if (!g_index_ready) {
+    if (!g_index_ready || g_index_root == NULL) {
         return -1;
     }
 
-    slot = (size_t) (hash_u64(id) % g_index.bucket_count);
-    while (scanned < g_index.bucket_count) {
-        if (!g_index.buckets[slot].used) {
-            return 1;
-        }
-        if (g_index.buckets[slot].id == id) {
-            *out_ref = g_index.buckets[slot].ref;
-            return 0;
-        }
-        slot = (slot + 1) % g_index.bucket_count;
-        scanned++;
-    }
-
-    return 1;
+    return bpt_find(g_index_root, id, out_ref);
 }
 
 uint64_t next_id(void) {
